0e985f2a2b7b01482f153dfb64becb54b5eecaf5
shakhaowat hossain <49072091+jebon000@users.noreply.github.com>
2019-04-06 16:03:09 +0530

    Update 18.c

--- 18.c
+++ 18.c
@@ -2,21 +2,21 @@
 #include<conio.h>
 void Sort();
 int n;
-struct process
+struct pro
 {
     int p_no;
-    int arrival_t,burst_t,ct,wait_t,taround_time,p;
+    int arr_t,bur_t,ct,wait_t,tar_time,p;
 	int flag;
 }p_list[100];
 void Sort()
 {
-    struct process p;
+    struct pro p;
     int i,j;
     for(i=0;i<n-1;i++)
     {
         for(j=i+1;j<n;j++)
         {
-            if(p_list[i].arrival_t >p_list[j].arrival_t)
+            if(p_list[i].arr_t >p_list[j].arr_t)
             {
                 p=p_list[i];
                 p_list[i]=p_list[j];
@@ -29,7 +29,7 @@ int main()
 {
     int i,t=0,b_t=0,peak;
     int a[10];
-    float wait_time=0,taround_time=0,avg_w_t=0,avg_taround_time=0;
+    float wait_time=0,tar_time=0,avg_w_t=0,avg_tar_time=0;
     printf("Enter the number of total processes\n");
     scanf("%d",&n);
     for(i=0;i<n;i++)
@@ -37,51 +37,51 @@ int main()
         p_list[i].p_no = i + 1;
         printf("Enter details For process %d\n",p_list[i].p_no);
         printf("Enter Arrival Time");
-        scanf("%d",&p_list[i].arrival_t);
+        scanf("%d",&p_list[i].arr_t);
         printf("Enter Burst Time");
-        scanf("%d", &p_list[i].burst_t);
+        scanf("%d", &p_list[i].bur_t);
         p_list[i].flag = 0;
-        b_t = b_t+p_list[i].burst_t;
+        b_t = b_t+p_list[i].bur_t;
     }
     Sort();
     for(i=0;i<n;i++)
     {
-        a[i]=p_list[i].burst_t;
+        a[i]=p_list[i].bur_t;
     }
-    p_list[9].burst_t=9999;
-    for(t=p_list[0].arrival_t;t<=b_t+1;)
+    p_list[9].bur_t=1111;
+    for(t=p_list[0].arr_t;t<=b_t+1;)
     {
         peak=9;
         for(i=0;i<n;i++)
         {
-            if(p_list[i].arrival_t<=t && p_list[i].burst_t<p_list[peak].burst_t && p_list[i].flag !=1)
+            if(p_list[i].arr_t<=t && p_list[i].bur_t<p_list[peak].bur_t && p_list[i].flag !=1)
             {
                 peak=i;
             }
-            if(p_list[peak].burst_t == 0 && p_list[i].flag !=1)
+            if(p_list[peak].bur_t == 0 && p_list[i].flag !=1)
             {
                 p_list[i].flag=1;
                 p_list[peak].ct=t;
-                p_list[peak].burst_t=9999;
+                p_list[peak].bur_t=1111;
                 printf("P%d completes in %d\n",p_list[i].p_no, p_list[peak].ct);
             }
         }
         t++;
-        (p_list[peak].burst_t)--;
+        (p_list[peak].bur_t)--;
     }
     for(i=0;i<n;i++)
     {
-        p_list[i].taround_time=(p_list[i].ct)-(p_list[i].arrival_t);
-        avg_taround_time=avg_taround_time+p_list[i].taround_time;
-        p_list[i].wait_t=((p_list[i].taround_time)-a[i]);
+        p_list[i].tar_time=(p_list[i].ct)-(p_list[i].arr_t);
+        avg_tar_time=avg_tar_time+p_list[i].tar_time;
+        p_list[i].wait_t=((p_list[i].tar_time)-a[i]);
         avg_w_t=avg_w_t+p_list[i].wait_t;
     }
     printf("PNO \tAT \tCT \tTA \tWT t\n");
     for(i=0;i<n;i++)
     {
-        printf("P%d\t%d\t%d\t%d\t%d\n",p_list[i].p_no,p_list[i].arrival_t,p_list[i].ct,p_list[i].taround_time,p_list[i].wait_t);
+        printf("P%d\t%d\t%d\t%d\t%d\n",p_list[i].p_no,p_list[i].arr_t,p_list[i].ct,p_list[i].tar_time,p_list[i].wait_t);
     }
-    printf("Average turn-around time%f\t\n\n",avg_taround_time);
+    printf("Average turn-around time%f\t\n\n",avg_tar_time);
     printf("Average waiting time\t %f\t\n",avg_w_t);
 	getch();
 }
