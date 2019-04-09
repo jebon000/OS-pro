d9da072c8f775a63a6998afbcce52d340f60c08f
shakhaowat hossain <49072091+jebon000@users.noreply.github.com>
2019-04-09 20:51:36 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -1,61 +1,88 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
 #include<conio.h>
 int main()
-{
-FILE *fp=fopen("cpu_burst.txt","r");
-int bt[20],p[20],wt[20],tat[20],i=0,j,n=5,total=0,pos,temp;
-float avg_wt,avg_tat;
-printf("\nReading CPU_BURST.txt File\n");
-for(i=0;i<5;i++)
-while((getc(fp))!=EOF)
-{
-fscanf(fp,"%d",&bt[i]);
-if(bt[i]>0)
-{
-p[i]=i+1;
-i++;}        
+ {
+   int x,n,p[10],pp[10],bt[10],w[10],t[10],awt,atat,i,at[10],tq;
+ 
+   //n is number of process
+   //p is process
+   //pp is process priority
+   //bt is process burst time
+   //w is wait time
+   // t is turnaround time
+   //awt is average waiting time
+   //atat is average turnaround time
+   //at is arrival time
+   //tq is time qunatum
+ 
+ 
+   printf("Enter the number of process : ");
+   scanf("%d",&n);
+   printf("Enter time quantum");
+   scanf("%d",&tq);
+   printf("\n\t Enter burst time : time priorities : Arrival time  \n");
+ 
+   for(i=0;i<n;i++)
+    {
+      //cout<<"\nProcess["<<i+1<<"]:";
+      printf("\n Process %d ",i+1);
+      //cin>>bt[i]>>pp[i]>>at[i];
+      scanf("%d %d %d",&bt[i],&pp[i],&at[i]);
+	  p[i]=i+1;
+    }
+ 
+//sorting on the basis of priority
+int j;
+  for(i=0;i<n-1;i++)
+   {
+     for(j=i+1;j<n;j++)
+       {
+     x=pp[i];
+     pp[i]=pp[j];
+     {
+       if(pp[i]<pp[j])
+     pp[j]=x;
+     x=bt[i];
+     bt[i]=bt[j];
+     bt[j]=x;
+     x=p[i];
+     p[i]=p[j];
+     p[j]=x;
+      }
+   }
 }
-n=i;
-for(i=0;i<n;i++)
-{
-pos=i;
-for(j=i+1;j<n;j++)
-{
-if(bt[j]<bt[pos])
-pos=j;
-}
-temp=bt[i];
-bt[i]=bt[pos];
-bt[pos]=temp;
-temp=p[i];
-p[i]=p[pos];
-p[pos]=temp;
-}
-wt[0]=0;            
+w[0]=0;
+awt=0;
+t[0]=bt[0];
+atat=t[0];
 for(i=1;i<n;i++)
-{
-wt[i]=0;
-for(j=0;j<i;j++)
-wt[i]+=bt[j];
-total+=wt[i];
+ {
+ 	if(tq<n || tq>n) {
+	 
+   w[i]=t[i-1];
+   awt+=w[i];
+   t[i]=w[i]+bt[i];
+   atat+=t[i];
+	 }
+ }
+ 
+ //Gantt chrat
+ printf("\n");
+ printf("Gantt chart\n");
+ for(i=0;i<n;i++)
+ {
+ 	printf("P %d ",p[i]);
+ }
+//Displaying the process
+ 
+printf("\n\nProcess \t Burst Time \t Wait Time \t Turn Around Time   Priority \tArrival time \n");
+for(i=0;i<n;i++){
+printf("\n  %d",p[i]); printf("\t\t %d",bt[i]); printf("\t\t %d",w[i]); printf("\t\t %d",t[i]); printf("\t\t %d",pp[i]); printf("\t\t %d",at[i]);
+
 }
-avg_wt=(float)total/n;      
-total=0;
-printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
-for(i=0;i<n;i++)
-{
-tat[i]=bt[i]+wt[i];     
-total+=tat[i];
-printf("\n p %d\t\t  %d\t\t    %d\t\t\t %d",p[i],bt[i],wt[i],tat[i]);
-}
-avg_tat=(float)total/n;     
-printf("\n\nAverage Waiting Time=%f",avg_wt);
-printf("\nAverage Turnaround Time=%f\n",avg_tat);
-fclose(fp);
-return 0;
+awt/=n;
+atat/=n;
+printf("\n Average Wait Time : %d ",awt);
+printf("\n Average Turn Around Time : %d",atat);
 getch();
 }
-
